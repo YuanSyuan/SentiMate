b802beae0b0aadf5108996752d8dc626b297a5d6
YuanSyuan <kelly.yuansyuan@gmail.com>
2024-04-10 16:07:38 +0800

    initial commit

---
+++ SentiMate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SentiMate
+//
+//  Created by 李芫萱 on 2024/4/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
