5fd5bc9bf04070ff60ed4476658f649c3d880f98
YuanSyuan <kelly.yuansyuan@gmail.com>
2024-04-11 19:10:42 +0800

    feat: IQKeyboard added

--- SentiMate/Controllers/PostDetailVC.swift
+++ SentiMate/Controllers/PostDetailVC.swift
@@ -6,10 +6,6 @@
 //
 
 import UIKit
-
-// Define cells
-
-
 class PostDetailVC: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
@@ -23,10 +19,6 @@ class PostDetailVC: UIViewController {
         tableView.delegate = self
         
     }
-    
-    // MARK: - Cell Arrangement
-  
-
 
 }
 
@@ -36,24 +28,50 @@ extension PostDetailVC: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        switch indexPath.row {
-        case 0:
-            let cell = tableView.dequeueReusableCell(withIdentifier: "date", for: indexPath) as! DateCell
-            return cell
-        case 1:
-            let cell = tableView.dequeueReusableCell(withIdentifier: "emotion", for: indexPath) as! EmotionCell
-            return cell
-        case 2:
-            let cell = tableView.dequeueReusableCell(withIdentifier: "category", for: indexPath) as! CategoryCell
-            return cell
-        default:
-            let cell = tableView.dequeueReusableCell(withIdentifier: "textfield", for: indexPath) as! TextFieldCell
-            return cell
+            
+            switch indexPath.row {
+            case 0:
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: "date", for: indexPath) as? DateCell else {
+                    fatalError("Could not dequeue DateCell")
+                    // there is date picker in this cell
+                }
+                return cell
+            case 1:
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: "emotion", for: indexPath) as? EmotionCell else {
+                    fatalError("Could not dequeue EmotionCell")
+                }
+                cell.configure(withEmotion: emotion ?? "")
+                return cell
+            case 2:
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: "category", for: indexPath) as? CategoryCell else {
+                    fatalError("Could not dequeue CategoryCell")
+                    // there is a chosen category btn in this cell
+                }
+                return cell
+            default:
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: "textfield", for: indexPath) as? TextFieldCell else {
+                    fatalError("Could not dequeue TextFieldCell")
+                    // there is text in this cell
+                    // there is a saveBtn in this cell, that handle save to firebase
+                }
+                return cell
+            }
         }
-    }
 }
 
 extension PostDetailVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.row {
+        case 0:
+            return 73
+        case 1:
+            return 126
+        case 2:
+            return 250
+        default:
+            return 291
+        }
+    }
     
 }
 
