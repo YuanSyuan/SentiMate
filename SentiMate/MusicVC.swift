c4042f62c11af41f5a2851d6afc6dd8607f2bc7a
YuanSyuan <kelly.yuansyuan@gmail.com>
2024-04-11 09:26:33 +0800

    feat: Add VCs

---
+++ SentiMate/MusicVC.swift
@@ -0,0 +1,19 @@
+//
+//  MusicVC.swift
+//  SentiMate
+//
+//  Created by 李芫萱 on 2024/4/10.
+//
+
+import UIKit
+
+class MusicVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- SentiMate/PostVC.swift
+++ SentiMate/PostVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  PostVC.swift
 //  SentiMate
 //
 //  Created by 李芫萱 on 2024/4/10.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PostVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,4 +16,3 @@ class ViewController: UIViewController {
 
 
 }
-
