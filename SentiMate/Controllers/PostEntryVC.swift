f19332527a7d802c0f6f893cab442898843ad88e
YuanSyuan <kelly.yuansyuan@gmail.com>
2024-04-20 20:02:40 +0800

    feat: add PostEntryVC

---
+++ SentiMate/Controllers/PostEntryVC.swift
@@ -0,0 +1,16 @@
+//
+//  PostEntryVC.swift
+//  SentiMate
+//
+//  Created by 李芫萱 on 2024/4/20.
+//
+
+import UIKit
+
+class PostEntryVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
